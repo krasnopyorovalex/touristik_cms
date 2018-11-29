9f018a9f05a38f142c0885894aa75999eed5e76a
Alex Krasnopyorov <krasnopyorovalex88@gmail.com>
2018-11-29 18:27:36 +0300

    add og properties: modify to main page

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -6,11 +6,7 @@
 <meta property="og:title" content="{{ $page->title }}">
     <meta property="og:type" content="website">
     <meta property="og:url" content="{{ request()->getUri() }}">
-@if ($page->image)
-    <meta property="og:image" content="{{ asset($page->image->path) }}">
-@else
     <meta property="og:image" content="{{ asset('img/logo_green.svg') }}">
-@endif
     <meta property="og:description" content="{{ $page->description }}">
     <meta property="og:site_name" content="Веб-студия Красбер в Крыму и Краснодарском крае">
     <meta property="og:locale" content="ru_RU">

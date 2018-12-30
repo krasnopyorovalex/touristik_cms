ce2e7ff6216e6b23ad8345f3a17b975ef240b19b
Alex Krasnopyorov <krasnopyorovalex88@gmail.com>
2018-12-30 11:10:13 +0300

    change faqs.balde.php

--- resources/views/layouts/shortcodes/faqs.blade.php
+++ resources/views/layouts/shortcodes/faqs.blade.php
@@ -3,7 +3,7 @@
         @foreach ($faqs as $faq)
             <li>
                 <div class="q">{{ $faq->question }}</div>
-                <div class="a">{!! strip_tags($faq->answer) !!}</div>
+                <div class="a">{!! $faq->answer !!}</div>
             </li>
         @endforeach
     </ul>

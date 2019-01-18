57bf1ddc68e9cb16cdb0b15d643d875b20173090
Alex Krasnopyorov <krasnopyorovalex88@gmail.com>
2019-01-18 10:20:36 +0300

    task #1373: add sub items in mobile menu

--- resources/views/layouts/menus/footer_mobile.blade.php
+++ resources/views/layouts/menus/footer_mobile.blade.php
@@ -3,7 +3,14 @@
         @foreach($menu->get('menu_header') as $item)
             <li{!! add_css_class($item) !!}>
                 <a itemprop="url" href="{{ $item->link }}">{{ $item->name }}</a>
-                @if (count($item->menuItems))<span><i class="icon__arrow"></i></span>@endif
+                @if (count($item->menuItems))
+                    <span><i class="icon__arrow"></i></span>
+                    <ul>
+                        @foreach($item->menuItems as $subItem)
+                            <li><a href="{{ $subItem->link }}">{{ $subItem->name }}</a></li>
+                        @endforeach
+                    </ul>
+                @endif
             </li>
         @endforeach
     </ul>

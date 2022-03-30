24081fed88f3d40ba701504c8fe69637935c0b43
sanek <krasnopyorovalex88@gmail.com>
2022-03-30 07:34:05 +0300

    update css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes"/>
-    <title>@yield('title', 'Мебель для гостиниц')</title>
+    <title>@yield('title', 'Бравый турист')</title>
     <meta name="description" content="@yield('description', '')">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="theme-color" content="#eee">

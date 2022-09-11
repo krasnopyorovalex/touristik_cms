1d80c75e376cacf724955b4d0fbfdd2ea831a7c8
Sanek Krasnopyorov <krasnopyorovalex88@gmail.com>
2022-09-11 14:40:30 +0300

    add schedule yandex links

--- resources/views/layouts/shortcodes/schedule_yandex.blade.php
+++ resources/views/layouts/shortcodes/schedule_yandex.blade.php
@@ -1,15 +1,15 @@
 @if($schedules->count())
 <div class="schedule schedule_yandex">
     <div class="schedule__row">
-        <div class="date" style="width: 25%">Срок истечения доступа на скачивание</div>
-        <div class="name" style="width: 50%">Название тура</div>
-        <div class="order" style="width: 15%">Ссылка для скачивания</div>
+        <div class="date">Срок истечения доступа на скачивание</div>
+        <div class="name">Название тура</div>
+        <div class="order">Ссылка для скачивания</div>
     </div>
     @foreach ($schedules as $schedule)
     <div class="schedule__row">
-        <div class="date" style="width: 25%">{{ $schedule->date_string ?: $schedule->finished_at->formatLocalized('%d %b %Y') }} г.</div>
-        <div class="name" style="width: 50%">{!! strip_tags($schedule->body, '<a>') !!}</div>
-        <div class="order" style="width: 15%"><a href="{{ $schedule->link_to_yandex_disk }}" target="_blank" class="btn">Скачать</a></div>
+        <div class="date">{{ $schedule->date_string ?: $schedule->finished_at->formatLocalized('%d %b %Y') }} г.</div>
+        <div class="name">{!! strip_tags($schedule->body, '<a>') !!}</div>
+        <div class="order"><a href="{{ $schedule->link_to_yandex_disk }}" target="_blank" class="btn">Скачать</a></div>
     </div>
     @endforeach
 </div>
